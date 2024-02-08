76e2656653e70ede47710f07ec4cff40dba6092a
Pedro <pedrogabrielcruzlima@gmail.com>
2024-02-08 20:44:51 -0300

    Adding dto's

---
+++ Application/DTOs/OrderDTO.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enums;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CustomerName { get; set; }
+        public List <ProductDTO> Products { get; set; }
+        public OrderType OrderType { get; set; }
+        public DeliveryAddress DeliveryAddress { get; set; }
+    }
+}

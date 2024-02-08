4caca4db416bc416ec0205e356102b13b24212a5
Pedro <pedrogabrielcruzlima@gmail.com>
2024-02-08 16:34:40 -0300

    Adição de repositorio generico

---
+++ Infrastructure/Data/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using Infrastructure.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class Repository<T> where T : class
+    {
+        protected readonly RestaurantDbContext _context;
+
+        public Repository(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            
+            _context.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+    }
+}
---
+++ Infrastructure/ExtensionMethods/CommonDependencyInject.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ExtensionMethods
+{
+    public static class CommonDependencyInject
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}

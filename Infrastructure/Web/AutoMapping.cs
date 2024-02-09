78f14b17262cca29a4310fc8879d78429d34ae5f
Pedro <pedrogabrielcruzlima@gmail.com>
2024-02-08 21:47:06 -0300

    Criação de classe de automapping

---
+++ Infrastructure/Web/AutoMapping.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Web
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            CreateMap<Order, OrderDTO>();
+            CreateMap<OrderDTO, Order>();
+            CreateMap<Product, ProductDTO>();
+            CreateMap<ProductDTO, Product>();
+        }
+    }
+}
